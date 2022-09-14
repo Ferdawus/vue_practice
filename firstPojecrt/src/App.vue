d5512915db917e55e001e06311eab77540e0df2a
Ferdawus <ferdawusa@gmail.com>
2022-09-13 18:40:55 -0700

    first commit

---
+++ firstPojecrt/src/App.vue
@@ -0,0 +1,22 @@
+<template lang="">
+  <div>
+    <h2>{{greet}} {{name}}</h2>
+  </div>
+  <div v-text="channel"></div>
+  <div v-text="name"></div>
+</template>
+<script>
+export default {
+  name:"App",
+  data() {
+    return {
+      greet: "Hello",
+      name:"M.Ferdawus",
+      channel:"Codevolution"
+    }
+  },
+}
+</script>
+<style lang="">
+  
+</style>
